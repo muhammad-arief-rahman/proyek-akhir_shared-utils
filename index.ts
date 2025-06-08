28c18d26ec0f203443d8e171d0fe2daf7241d4e4
Muhammad Arief Rahman <arief21si@mahasiswa.pcr.ac.id>
2025-06-08 22:18:59 +0700

    add rest parameters to response

--- index.ts
+++ index.ts
@@ -37,11 +37,12 @@ export function response(
   res: Parameters<RequestHandler>[1],
   status = 200,
   message: string,
-  data: any = null
+  data: any = null,
+  rest: Record<string, any> = {}
 ) {
   if (status >= 200 && status < 400) {
-    return res.status(status).json({ status, message, data })
+    return res.status(status).json({ status, message, data, ...rest })
   } else {
-    return res.status(status).json({ status, message, error: data })
+    return res.status(status).json({ status, message, error: data, ...rest })
   }
 }
