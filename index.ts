d51fa435250724e9beb1ab937b9cc32abd16f1d3
Muhammad Arief Rahman <arief21si@mahasiswa.pcr.ac.id>
2025-06-11 19:58:40 +0700

    add pagination creation function

--- index.ts
+++ index.ts
@@ -3,8 +3,13 @@
 import type { RequestHandler } from "express"
 import { ZodError } from "zod"
 
-export function internalServerError(res: Parameters<RequestHandler>[1], error?: any) {
-  return res.status(500).json({ message: "Internal server error", error: error })
+export function internalServerError(
+  res: Parameters<RequestHandler>[1],
+  error?: any
+) {
+  return res
+    .status(500)
+    .json({ message: "Internal server error", error: error })
 }
 
 export function zodHandler(error: ZodError) {
@@ -45,4 +50,45 @@ export function response(
   } else {
     return res.status(status).json({ status, message, error: data, ...rest })
   }
+}
+
+export function createResponsePagination({
+  data = [],
+  page = 1,
+  totalData = 1,
+}: {
+  totalData: number
+  page: string | number
+  data: any[]
+}): PaginationResponse {
+  return {
+    currentPage: Number(page),
+    totalPages: totalData,
+    totalItems: data.length,
+    canGoNext: Number(page) < totalData,
+    canGoBack: Number(page) > 1,
+    hasManyNext: Number(page) + 2 < totalData, // If the current page + 2 is less than total pages, it means there are more pages after the current page
+    hasManyBack: Number(page) - 2 > 1, // If the current page - 1 is greater than 1, it means there are more pages before the current page
+
+    // Get 2 page numbers before and after the current page and the current page itself
+    pageNumbers: [
+      ...Array.from({ length: 2 }, (_, i) => Math.max(1, Number(page) - 2 + i)),
+      Number(page),
+      ...Array.from({ length: 2 }, (_, i) =>
+        Math.min(totalData, Number(page) + i + 1)
+      ),
+    ].filter((num, index, self) => self.indexOf(num) === index), // Remove duplicates
+  }
+}
+
+// * TYPES
+export type PaginationResponse = {
+  currentPage: number
+  totalPages: number
+  totalItems: number
+  canGoNext: boolean
+  canGoBack: boolean
+  hasManyNext: boolean
+  hasManyBack: boolean
+  pageNumbers: number[]
 }
