6310c6edf61d54df34912500adf4b0b0064c2ff0
Muhammad Arief Rahman <arief21si@mahasiswa.pcr.ac.id>
2025-07-01 08:48:01 +0700

    feat: add verifyJwt utility to common exports and update version to 0.6.1

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ export {
   zodCatchHandler,
   zodHandler,
   getAuthToken,
+  verifyJwt
 } from "./src/utils"
 
 // * Configs
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,6 @@ export { default as zodCatchHandler } from "./zod-catch-handler"
 export { default as response } from "./response"
 export { default as createResponsePagination } from "./create-response-pagination"
 export { default as getAuthToken } from "./get-auth-token"
+export { default as verifyJwt } from "./verify-jwt"
 
 export { default as AuthMiddleware } from "./auth-middleware"
